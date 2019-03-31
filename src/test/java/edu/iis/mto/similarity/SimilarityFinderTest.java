8e2114090978532b8fe25eae41044a28e1d1b9db
209347 <209347@edu.p.lodz.pl>
2019-04-01 00:14:07 +0200

    2. Added jackardSimilarityShouldBeOneIfSequencesAreEqual, removed code duplicates and unnecessary code.

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -8,16 +8,23 @@ import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
 
+    private final SequenceSearcherDoubler sequenceSearcherDoubler = new SequenceSearcherDoubler();
+
+    public final SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDoubler);
+
     @Test
     public void jackardSimilarityTest() {
-        SequenceSearcherDoubler sequenceSearcherDoubler = new SequenceSearcherDoubler();
-        SimilarityFinder similarityFinder = new SimilarityFinder(sequenceSearcherDoubler);
-
         int[] seq1 = {1, 2, 3};
         int[] seq2 = {4, 2, 5};
 
-        similarityFinder.calculateJackardSimilarity(seq1, seq2);
         //3 + 3 - 1 = 5 => jackardSimilarity = 1.0/5.0 = 0.2
         Assert.assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq2), Matchers.equalTo(0.2));
+    }
+
+    @Test
+    public void jackardSimilarityShouldBeOneIfSequencesAreEqualTest() {
+        int[] seq1 = {1, 2, 3};
+
+        Assert.assertThat(similarityFinder.calculateJackardSimilarity(seq1, seq1), Matchers.equalTo(1.0));
     }
 }
